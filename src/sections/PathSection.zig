fcdca3eba4b23b83741d700c5da7c0f0d173ff4d
Kacaii <kacaii.privado@gmail.com>
2025-03-11 19:59:58 -0300

    change path to blue

--- src/sections/PathSection.zig
+++ src/sections/PathSection.zig
@@ -6,7 +6,7 @@ const set_color = struct {
     const normal = "\x1b[39m";
 };
 
-/// Returns the path section.
+/// Returns the path for the current working directory.
 /// Caller owns the memory
 pub fn init(allocator: Allocator, dir: std.fs.Dir) ![]const u8 {
     const real_path = try dir.realpathAlloc(allocator, ".");
@@ -24,11 +24,7 @@ pub fn init(allocator: Allocator, dir: std.fs.Dir) ![]const u8 {
     const path = std.mem.concat(
         allocator,
         u8,
-        &[_][]const u8{
-            set_color.normal,
-            parsed_path,
-            set_color.normal,
-        },
+        &[_][]const u8{ set_color.blue, parsed_path, set_color.normal },
     );
 
     return path;
