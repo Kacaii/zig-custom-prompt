83c31647602680e9a78806b5eb32a3c696a28135
Kacaii <kacaii.privado@gmail.com>
2025-03-13 12:27:50 -0300

    add unit workspace section unit tests

--- build.zig
+++ build.zig
@@ -99,6 +99,18 @@ pub fn build(b: *std.Build) void {
         .root_module = lib_mod,
     });
 
+    const workspace_section_mod = b.createModule(.{
+        .root_source_file = b.path("src/sections/workspaces/Workspace.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const workspace_section_unit_tests = b.addTest(
+        .{ .root_module = workspace_section_mod },
+    );
+
+    const run_workspace_section_unit_tests = b.addRunArtifact(workspace_section_unit_tests);
+
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const exe_unit_tests = b.addTest(.{
@@ -113,4 +125,5 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_workspace_section_unit_tests.step);
 }
