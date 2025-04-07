2156cae1a30cb90eda5945ff6fadd0eb09e2b603
Kacaii <kacaii.privado@gmail.com>
2025-04-07 20:07:47 -0300

    rename allocator to gpa

--- src/main.zig
+++ src/main.zig
@@ -7,14 +7,14 @@ const HostSection = @import("./sections/HostSection.zig");
 const PathSection = @import("./sections/PathSection.zig");
 const GitSection = @import("./sections/WorkspaceSection/GitSection.zig");
 const WorkspaceSection = @import("./sections/WorkspaceSection/WorkspaceSection.zig");
-const ArrowSection = @import("./sections/ArrowSection.zig");
+const PromptSymbol = @import("./sections/PromptSymbolSection.zig");
 
 pub fn main() !void {
     const cwd = fs.cwd();
     const stdout = std.io.getStdOut().writer();
 
     var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
-    const allocator, const is_debug = switch (builtin.mode) {
+    const gpa, const is_debug = switch (builtin.mode) {
         .Debug, .ReleaseSafe => .{ debug_allocator.allocator(), true },
         .ReleaseFast, .ReleaseSmall => .{ std.heap.smp_allocator, false },
     };
@@ -24,32 +24,27 @@ pub fn main() !void {
     };
 
     // Host name
-    const host_section = HostSection.init(allocator) catch "";
-    defer allocator.free(host_section);
+    const host_section = HostSection.init(gpa) catch "";
+    defer gpa.free(host_section);
 
     // Icon and Programming Language
-    const workspace_section = WorkspaceSection.init(allocator, cwd) catch "";
-    defer allocator.free(workspace_section);
+    const workspace_section = WorkspaceSection.init(gpa, cwd) catch "";
+    defer gpa.free(workspace_section);
 
     // Git Branch
-    const git_section = GitSection.init(allocator) catch "";
-    defer allocator.free(git_section);
+    const git_section = GitSection.init(gpa) catch "";
+    defer gpa.free(git_section);
 
     // Path
-    const path_section = PathSection.init(allocator, cwd) catch "";
-    defer allocator.free(path_section);
+    const path_section = PathSection.init(gpa, cwd) catch "";
+    defer gpa.free(path_section);
 
-    const arrow_section = try ArrowSection.init(allocator);
-    defer allocator.free(arrow_section);
+    // Arrow
+    const arrow_section = PromptSymbol.init(gpa) catch " $ ";
+    defer gpa.free(arrow_section);
 
     _ = try stdout.print(
         " {s} | {s} | {s}{s} \n {s} ",
-        .{
-            host_section,
-            workspace_section,
-            git_section,
-            path_section,
-            arrow_section,
-        },
+        .{ host_section, workspace_section, git_section, path_section, arrow_section },
     );
 }
--- src/sections/PromptSymbolSection.zig
+++ src/sections/PromptSymbolSection.zig
@@ -1,3 +1,5 @@
+//! Handles aquiring a prompt symbol, usually "$".
+
 const std = @import("std");
 
 const set_color = struct {
@@ -5,6 +7,8 @@ const set_color = struct {
     const default = "\x1b[39m";
 };
 
+/// Returns a simple prompt symbol.
+/// Caller owns the memory.
 pub fn init(allocator: std.mem.Allocator) ![]const u8 {
     return try std.fmt.allocPrint(
         allocator,
